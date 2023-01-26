6f69da598bf0438114c679da47dae23e7d00eec9
Elijah <chilesheelijah01@gmail.com>
2023-01-26 15:54:59 +0200

    Created Application Structure

--- App.js
+++ App.js
@@ -4,8 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Welcome Elijah</Text>
+      </View>
+      {/* body */}
+      <View style={styles.body}>
+        <Text>Greeen Ejay </Text>
+        <Text>Greeen Ejay </Text>
+        <Text>Greeen Ejay </Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +24,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor:'gray',
+    padding: '20px'
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: 'green'
+  }
 });
