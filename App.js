2d2eee89abc68938bb0e4cd28c28cde84c14e677
Elijah <chilesheelijah01@gmail.com>
2023-02-05 12:07:16 +0200

    Added  Todo form and View Page

--- App.js
+++ App.js
@@ -1,18 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+
+// Components
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import AddTodos from './components/AddTodos';
+
 
 export default function App() {
+
+  const [todos, setTodos] = useState([ 
+    {text: 'Buy Coffee', key:'1'},
+    {text: 'Drink Water', key:'2'},
+    {text: 'Have Lunch', key:'3'}
+  ])
+
+const pressHandler = (key) => {
+  setTodos((prevTodos) => {
+    return prevTodos.filter(todo => todo.key != key)
+  })
+}
+
+const submitHandler = (text) => {
+  setTodos((prevTodos) => {
+    return [
+      {text: text, key: Math.random().toString()},
+      ...prevTodos
+    ]
+  })
+}
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Welcome Elijah</Text>
+      {/* Header */}
+      <Header />
+      <View style={styles.content}>
+        {/* to form */}
+        <AddTodos submitHandler={submitHandler}/>
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
       </View>
-      {/* body */}
-      <View style={styles.body}>
-        <Text>Greeen Ejay </Text>
-        <Text>Greeen Ejay </Text>
-        <Text>Greeen Ejay </Text>
-      </View>
+        
     </View>
   );
 }
@@ -21,17 +55,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  header: {
-    backgroundColor:'gray',
-    padding: '20px'
+  content: {
+    padding: 40
   },
-  boldText: {
-    fontWeight: 'bold'
-  },
-  body: {
-    backgroundColor: 'green'
+  list: {
+    marginTop: 20
   }
 });
